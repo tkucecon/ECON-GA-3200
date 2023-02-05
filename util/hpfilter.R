e628cff14318776fd5804f6a995a7404629f4322
tkucecon <takeuchi.takanori.44c@gmail.com>
2023-02-05 17:07:48 -0500

    amended so that you can get the results as figures or data frames

--- util/hpfilter.R
+++ util/hpfilter.R
@@ -2,24 +2,26 @@
 # ------------------------------------------------------------------------------
 # About this code
 # written by Takanori Takeuchi(tt2292@nyu.edu)
-# how to recycle the python code used in the util folder
+# conduct HP filtering
 # ------------------------------------------------------------------------------
 
 # set up------------------------------------------------------------------------
 
 # library
   library("tidyverse")
-  library("reticulate")
   library("lubridate")
   library("mFilter")
   library("ggthemes")
   theme_set(theme_solarized())
   
   options(warn = -1)
+  
+  # safunc needed to run this code:
+  # source("safunc.R")
 
 # create a function
   
-plot_hp <- function(df, ctry, indicator, freq, save){
+plot_hp <- function(df, ctry, indicator, freq, save, out.type){
   
   # drop NA rows in advance
   df <- 
@@ -43,11 +45,8 @@ plot_hp <- function(df, ctry, indicator, freq, save){
     unlist() %>% 
     as.numeric() %>% 
     ts(frequency = freq)
-  
-  ts.stl <- stl(ts.nsa, s.window = "periodic")
-  
-  # Save the seasonally adjusted data
-  ts.sa <- ts.stl$time.series[,2] + ts.stl$time.series[,3]
+
+  ts.sa <- sa_adj(ts.nsa)
   
   # Conduct HP filter
   ts.filtered <- hpfilter(log(ts.sa), freq = lamb, type = "lambda")
@@ -67,28 +66,36 @@ plot_hp <- function(df, ctry, indicator, freq, save){
     rename(trend = 'Series 1')
   
   # Plot
-  g.cycle <- 
+  g.gap <- 
     df.plot %>% 
     ggplot(aes(x = date, y = cycle)) +
-    geom_line() +
-    geom_hline(yintercept = 0) +
+    geom_line(linewidth = 0.7) +
+    geom_hline(yintercept = 0, linewidth = 0.5) +
     labs(y = "% deviation from trend")
   
-  g.compare <- 
+  g.trend <- 
     df.plot %>% 
     select(-cycle) %>% 
     gather(key = "key", value = "value", actual, trend) %>% 
     ggplot(aes(x = date, y = value, color = key)) + 
-    geom_line() + 
+    geom_line(linewidth = 0.7) + 
     labs(color = NULL) + 
     theme(legend.position = c(0.15, 0.85))
   
   if (save) {
-    ggsave(plot = g.cycle,   width = 5, height = 4, filename = paste("../6_outputs/", ctry, indicator, "_cyc.pdf", sep = ""))
-    ggsave(plot = g.compare, width = 5, height = 4, filename = paste("../6_outputs/", ctry, indicator, "_comp.pdf", sep = ""))
+    ggsave(plot = g.gap,   width = 5, height = 4, filename = paste("../6_outputs/hp_", ctry, indicator, "_gap.pdf", sep = ""))
+    ggsave(plot = g.trend, width = 5, height = 4, filename = paste("../6_outputs/hp_", ctry, indicator, "_trend.pdf", sep = ""))
   }
   
-  return(list(g.cycle, g.compare))
+  if (out.type == "data") {
+    return(df.plot)
+  } else if (out.type == "figure") {
+    return(list(g.gap, g.trend))
+  } else if (out.type == "both"){
+    return(list(df.plot, g.gap, g.trend))
+  } else {
+    print("out.type should be data, figure or both.")
+  }
 }
 
 
